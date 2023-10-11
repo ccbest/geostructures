055e59dc5bdaffd2777e4e44dcd7553486f73515
ccbest <carlcbest@gmail.com>
2023-10-11 08:33:34 -0500

    Niemeyer Geohashing (#50)
    Implements Niemeyer geohashing

--- geostructures/calc.py
+++ geostructures/calc.py
@@ -227,7 +227,7 @@ def do_vertices_intersect(
                 continue
 
             intersection = find_line_intersection(active_event.segment, event.segment)
-            if intersection and not intersection[1]:
+            if intersection:
                 return True
 
         active_events.add(event)
@@ -301,6 +301,20 @@ def find_line_intersection(
     def det(a, b):
         return a[0] * b[1] - a[1] * b[0]
 
+    def get_line_bounds(
+            line: Tuple[Tuple[float, float], Tuple[float, float]]
+    ) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+        """Organizes line bounding points into min/max x and y values"""
+        x_sort, y_sort = sorted([line[0][0], line[1][0]]), sorted([line[0][1], line[1][1]])
+        return (
+            (round_half_up(x_sort[0], 10), round_half_up(x_sort[1], 10)),
+            (round_half_up(y_sort[0], 10), round_half_up(y_sort[1], 10)),
+        )
+
+    def test_ranges_overlap(range1: Tuple[float, float], range2: Tuple[float, float]):
+        """Test whether two ranges overlap"""
+        return max([range1[0], range2[0]]) <= min([range1[1], range2[1]])
+
     line1_flt = (line1[0].to_float(), line1[1].to_float())
     if line1_flt[1][0] < line1_flt[0][0]:  # Flip order such that lower x value is first
         line1_flt = (line1_flt[1], line1_flt[0])
@@ -309,22 +323,11 @@ def find_line_intersection(
     if line2_flt[1][0] < line2_flt[0][0]:
         line2_flt = (line2_flt[1], line2_flt[0])
 
-    line1_y_bounds = (
-        min([line1_flt[0][1], line1_flt[1][1]]),
-        max([line1_flt[0][1], line1_flt[1][1]])
-    )
-    line2_y_bounds = (
-        min([line2_flt[0][1], line2_flt[1][1]]),
-        max([line2_flt[0][1], line2_flt[1][1]])
-    )
+    line1_bounds, line2_bounds = get_line_bounds(line1_flt), get_line_bounds(line2_flt)
 
     if not (
-            max([line1_flt[0][0], line2_flt[0][0]]) <= min([line1_flt[1][0], line2_flt[1][0]]) and
-            max(
-                [line1_y_bounds[0], line2_y_bounds[0]]
-            ) <= min(
-                [line1_y_bounds[1], line2_y_bounds[1]]
-            )
+        test_ranges_overlap(line1_bounds[0], line2_bounds[0]) and
+        test_ranges_overlap(line1_bounds[1], line2_bounds[1])
     ):
         # line bounds do not overlap
         return None
@@ -341,23 +344,17 @@ def find_line_intersection(
     x_intersection = round_half_up(det(d, xdiff) / div, 10)
     y_intersection = round_half_up(det(d, ydiff) / div, 10)
 
-    # Check if any of the x values are exactly the same - could be boundary intersection
-    if (x_intersection, y_intersection) in (*line1_flt, *line2_flt):
-        # Intersection exactly on one of the coordinates - boundary intersection
-        return Coordinate(x_intersection, y_intersection), True
-
     if (
-            max(
-                [line1_flt[0][0], line2_flt[0][0]]
-            ) <= x_intersection <= min(
-                [line1_flt[1][0], line2_flt[1][0]]
-            )
-            and max(
-                [line1_y_bounds[0], line2_y_bounds[0]]
-            ) <= y_intersection <= min(
-                [line1_y_bounds[1], line2_y_bounds[1]]
-            )
+        line1_bounds[0][0] <= x_intersection <= line1_bounds[0][1] and
+        line2_bounds[0][0] <= x_intersection <= line2_bounds[0][1] and
+        line1_bounds[1][0] <= y_intersection <= line1_bounds[1][1] and
+        line2_bounds[1][0] <= y_intersection <= line2_bounds[1][1]
     ):
-        return Coordinate(x_intersection, y_intersection), False
+        return (
+            Coordinate(x_intersection, y_intersection),
+
+            # If intersecting point is a line endpoint, is a boundary intersection
+            (x_intersection, y_intersection) in (*line1_flt, *line2_flt)
+        )
 
     return None
--- geostructures/geohash.py
+++ geostructures/geohash.py
@@ -5,13 +5,219 @@ Module for geohash transformers
 __all__ = ['H3Hasher', 'Hasher']
 
 import abc
-from collections import defaultdict
-from typing import Dict, Optional, Set
+from collections import defaultdict, Counter
+from typing import Dict, List, Optional, Set, Tuple, TypedDict
 
-from geostructures import Coordinate, GeoPoint, GeoLineString
+from geostructures import Coordinate, GeoBox, GeoLineString, GeoPoint
 from geostructures.structures import GeoShape
 from geostructures.collections import ShapeCollection
 from geostructures.calc import find_line_intersection
+
+
+_NIEMEYER_CONFIG_TYPE = TypedDict(
+    '_NIEMEYER_CONFIG_TYPE',
+    {
+        'bits': Tuple,
+        'charset': str,
+        'inverse': Dict[int, int],
+        'min_y': float,
+        'max_y': float,
+        'min_x': float,
+        'max_x': float,
+    }
+)
+
+_NIEMEYER_CONFIG: Dict[int, _NIEMEYER_CONFIG_TYPE] = {
+    16: {
+        'bits': (8, 4, 2, 1),
+        'charset': '0123456789abcdef',
+        'inverse': {
+            **{x + 48: x for x in range(10)},
+            **{x + 97: x + 10 for x in range(6)},
+        },
+        'min_y': -180.,
+        'max_y': 180.,
+        'min_x': -180.,
+        'max_x': 180.
+    },
+    32: {
+        'bits': (16, 8, 4, 2, 1),
+        'charset': '0123456789bcdefghjkmnpqrstuvwxyz',
+        'inverse': {
+            **{x + 48: x for x in range(10)},
+            **{x + 98: x + 10 for x in range(7)},
+            **{106: 17, 107: 18, 109: 19, 110: 20},
+            **{x + 112: x + 21 for x in range(11)}
+        },
+        'min_y': -90.,
+        'max_y': 90.,
+        'min_x': -180.,
+        'max_x': 180.
+    },
+    64: {
+        'bits': (32, 16, 8, 4, 2, 1),
+        'charset': '0123456789=ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz',
+        'inverse': {
+            **{x + 48: x for x in range(10)},
+            **{x + 65: x + 11 for x in range(26)},
+            **{x + 97: x + 38 for x in range(26)},
+            **{61: 10, 95: 37}
+        },
+        'min_y': -180.,
+        'max_y': 180.,
+        'min_x': -180.,
+        'max_x': 180.
+    }
+}
+
+
+def _decode_niemeyer(geohash: str, base: int) -> Tuple[float, float, float, float]:
+    """
+    Converts a Niemeyer geohash into the center lon/lat with corresponding error margins
+    Args:
+        geohash:
+            A geohash
+
+        base:
+            The geohash base; one of 16, 32, or 64
+
+    Returns:
+        longitude, latitude, longitude_error, latitude_error
+    """
+
+    config = _NIEMEYER_CONFIG[base]
+    lat_interval = [config['min_y'], config['max_y']]
+    lon_interval = [config['min_x'], config['max_x']]
+    lon_error, lat_error = config['max_x'], config['max_y']
+    lon_component = True
+
+    for character in geohash:
+        if character not in config['charset']:
+            raise ValueError(f'invalid character in geohash: {character}')
+
+        character_decoded = config['inverse'][ord(character)]
+        for mask in config['bits']:
+            if lon_component:
+                lon_error /= 2.0
+                if character_decoded & mask != 0:
+                    lon_interval[0] = (lon_interval[0] + lon_interval[1]) / 2.0
+                else:
+                    lon_interval[1] = (lon_interval[0] + lon_interval[1]) / 2.0
+            else:
+                lat_error /= 2.0
+                if character_decoded & mask != 0:
+                    lat_interval[0] = (lat_interval[0] + lat_interval[1]) / 2.0
+                else:
+                    lat_interval[1] = (lat_interval[0] + lat_interval[1]) / 2.0
+            lon_component = not lon_component
+
+    lat = (lat_interval[0] + lat_interval[1]) / 2.0
+    lon = (lon_interval[0] + lon_interval[1]) / 2.0
+
+    return lon, lat, lon_error, lat_error
+
+
+def coord_to_niemeyer(coordinate: Coordinate, length: int, base: int) -> str:
+    """
+    Find the geohash (of a specific base/length) in which a lat/lon point falls.
+
+    Args:
+        coordinate:
+            The coordinate to encode
+
+        length:
+            length of geohash
+
+        base:
+            the base of the geohash; one of 16, 32, 64
+
+    Return:
+        (str) the geohash in which the point falls
+    """
+    if base not in _NIEMEYER_CONFIG:
+        raise ValueError('Unsupported base, must be one of: 16, 32, 64')
+
+    config = _NIEMEYER_CONFIG[base]
+    geohash = ''
+    lat_interval = [config['min_y'], config['max_y']]
+    lon_interval = [config['min_x'], config['max_x']]
+    character, bit = 0, 0
+    lon_component = True
+    lon, lat = coordinate.to_float()
+
+    geohash_position = 0
+    while geohash_position < length:
+        if lon_component:
+            mid = (lon_interval[0] + lon_interval[1]) / 2.0
+            if lon > mid:
+                character |= config['bits'][bit]
+                lon_interval[0] = mid
+            else:
+                lon_interval[1] = mid
+        else:
+            mid = (lat_interval[0] + lat_interval[1]) / 2.0
+            if lat > mid:
+                character |= config['bits'][bit]
+                lat_interval[0] = mid
+            else:
+                lat_interval[1] = mid
+
+        if bit < len(config['bits']) - 1:
+            bit += 1
+        else:
+            geohash += config['charset'][character]
+            geohash_position += 1
+            character, bit = 0, 0
+
+        lon_component = not lon_component
+
+    return geohash
+
+
+def get_niemeyer_subhashes(geohash: str, base: int) -> Set[str]:
+    """
+    Given a Niemeyer geohash and its base, return the subhashes.
+
+    Args:
+        geohash:
+            A Niemeyer geohash
+
+        base:
+            the base of the geohash; one of 16, 32, 64
+
+    Returns:
+
+    """
+    if base not in _NIEMEYER_CONFIG:
+        raise ValueError('Unsupported base, must be one of: 16, 32, 64')
+
+    config = _NIEMEYER_CONFIG[base]
+    return {geohash + char for char in config['charset']}
+
+
+def niemeyer_to_geobox(geohash: str, base: int) -> GeoBox:
+    """
+    Convert a Niemeyer geohash to its representative rectangle (a centroid with
+    a corresponding error margin).
+
+    Args:
+        geohash:
+            A Niemeyer geohash
+
+        base:
+            the base of the geohash; one of 16, 32, 64
+
+    Return:
+        (float) center of geohash latitude
+        (float) center of geohash longitude
+        (float) height of the geohash in degrees latitude
+        (float) width of the geohash in degrees longitude
+    """
+    lon, lat, lon_error, lat_error = _decode_niemeyer(geohash, base)
+    return GeoBox(
+        Coordinate(lon - lon_error, lat + lat_error),
+        Coordinate(lon + lon_error, lat - lat_error)
+    )
 
 
 class Hasher(abc.ABC):
@@ -226,3 +432,165 @@ class H3Hasher(Hasher):
             return self._hash_linestring(shape, resolution)
 
         return self._hash_polygon(shape, resolution)
+
+
+class NiemeyerHasher(Hasher):
+
+    """
+    Converts geoshapes or collections of geoshapes into Niemeyer geohashes.
+
+    Args:
+        length:
+            The length of the Niemeyer hashes to return. Longer length geohashes
+            equate to smaller geospatial areas.
+
+        base:
+            The geohash algorithm base, one of 16, 32, or 64. The base determines the
+            number of subdivisions made when traversing from one geohash to its subordinate
+            geohashes.
+    """
+
+    def __init__(self, length: int, base: int):
+        self.length = length
+        self.base = base
+
+    @staticmethod
+    def _get_surrounding(geohash: str, base: int) -> List[str]:
+        """
+        Find all geohashes surrounding the geohash with the same base.
+
+        Args:
+            geohash: the central geohash
+            base: the base of the geohash
+
+        Return:
+            (List[str]) surrounding geohashes
+        """
+        length = len(geohash)
+        lon, lat, lon_err, lat_err = _decode_niemeyer(geohash, base)
+
+        return [
+            # from directly above, then clockwise
+            coord_to_niemeyer(Coordinate(lon, lat + lat_err * 2), length, base),
+            coord_to_niemeyer(Coordinate(lon + lon_err * 2, lat + lat_err * 2), length, base),
+            coord_to_niemeyer(Coordinate(lon + lon_err * 2, lat), length, base),
+            coord_to_niemeyer(Coordinate(lon + lon_err * 2, lat - lat_err * 2), length, base),
+            coord_to_niemeyer(Coordinate(lon, lat - lat_err * 2), length, base),
+            coord_to_niemeyer(Coordinate(lon - lon_err * 2, lat - lat_err * 2), length, base),
+            coord_to_niemeyer(Coordinate(lon - lon_err * 2, lat), length, base),
+            coord_to_niemeyer(Coordinate(lon - lon_err * 2, lat + lat_err * 2), length, base),
+        ]
+
+    def _hash_linestring(
+        self,
+        linestring: GeoLineString
+    ):
+        """
+        Find the geohashes that fall along a linestring.
+
+        Args:
+            linestring:
+                A geostructures.GeoLineString
+
+        Returns:
+            A set of geohashes
+        """
+        valid, checked, queue = set(), set(), set()
+        start = coord_to_niemeyer(linestring.coords[0], self.length, self.base)
+        queue.add(start)
+
+        while queue:
+            gh = queue.pop()
+            for near_gh in self._get_surrounding(gh, self.base):
+                if near_gh in checked:
+                    continue
+
+                checked.add(near_gh)
+                if niemeyer_to_geobox(near_gh, self.base).intersects(linestring):
+                    valid.add(near_gh)
+                    queue.add(near_gh)
+
+        return valid
+
+    def _hash_point(
+        self,
+        point: GeoPoint
+    ) -> Set[str]:
+        """
+        Find the geohash that corresponds to a point.
+
+        Args:
+            point:
+                A geostructures.GeoPoint
+
+        Returns:
+            A set of geohashes
+        """
+        return {coord_to_niemeyer(point.centroid, self.length, self.base)}
+
+    def _hash_polygon(
+        self,
+        polygon: GeoShape
+    ) -> Set[str]:
+        """
+        Find all geohashes that cover the polygon.
+
+        Args:
+            polygon: polygon to cover with geohashes
+
+        Returns:
+            (Set[str]) the geohashes that cover the polygon
+        """
+        valid, checked, queue = set(), set(), set()
+        start = coord_to_niemeyer(polygon.bounding_coords()[0], self.length, self.base)
+        queue.add(start)
+
+        while queue:
+            gh = queue.pop()
+            for near_gh in self._get_surrounding(gh, self.base):
+                if near_gh in checked:
+                    continue
+
+                checked.add(near_gh)
+                if niemeyer_to_geobox(near_gh, self.base).intersects(polygon):
+                    valid.add(near_gh)
+                    queue.add(near_gh)
+
+        return valid
+
+    def hash_collection(self, collection: ShapeCollection, **_):
+        """
+        Hash a geostructures FeatureCollection. Returns a dictionary of hashes and
+        the corresponding number of shapes the hash has been observed in.
+
+        Args:
+            collection:
+                A geostructures FeatureCollection
+
+        Returns:
+            dict
+        """
+        counter: Counter = Counter()
+        for shape in collection.geoshapes:
+            counter.update(self.hash_shape(shape))
+
+        return dict(counter)
+
+    def hash_shape(self, shape: GeoShape, **_):
+        """
+        Converts a geoshape into a set of geohashes that make up the shape.
+
+        Args:
+            shape:
+                A geostructures shape
+
+        Returns:
+            set
+        """
+        if isinstance(shape, GeoPoint):
+            return self._hash_point(shape)
+
+        if isinstance(shape, GeoLineString):
+            return self._hash_linestring(shape)
+
+        return self._hash_polygon(shape)
