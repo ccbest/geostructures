3ce023bd372f4aa8fa106d6bd78f4214f8ddff04
Aaron Hauptmann <aahauptmann@gmail.com>
2024-02-16 13:39:17 -0500

    Fixed linting 2

--- geostructures/utils/agg_functions.py
+++ geostructures/utils/agg_functions.py
@@ -5,13 +5,13 @@ __all__ = ['total_time', 'unique_entities']
 from typing import List
 from geostructures.structures import GeoShape
 
+
 def total_time(shapes: List[GeoShape]) -> float:
     """If the hash is larger than the data, this is time spend in each hash in seconds.
     If the hash is smaller than the data, this is time that could have been spent in
     each hash in seconds."""
     return (sum([(shape.dt.end - shape.dt.start).total_seconds()
                  if shape.dt else 0 for shape in shapes]))
-
 
 def unique_entities(shapes: List[GeoShape]) -> float:
     """If each shape is attributed to an entity in the properties field, this counts the
