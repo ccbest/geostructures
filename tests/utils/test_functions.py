84fd501f071b6500755a02bd1454a9c55e47544c
Eli Talbert <etalbert102@gmail.com>
2024-04-30 14:49:41 -0400

    replace with a faster version of round half up

--- tests/utils/test_functions.py
+++ tests/utils/test_functions.py
@@ -1,13 +1,6 @@
 
 from geostructures.utils.functions import *
 
-
-def test_float_to_str():
-    assert float_to_str(1.02) == '1.02'
-    assert float_to_str(0.00000000000000000001) == '0.00000000000000000001'
-    assert float_to_str(100000000000000000000.) == '100000000000000000000'
-
-
 def test_round_half_up():
     assert round_half_up(1.59, 1) == 1.6
     assert round_half_up(1.51, 1) == 1.5
