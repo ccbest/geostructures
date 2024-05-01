a9ba8439c28d0402a6cf592d9d6308aa2f7e62ff
Eli Talbert <etalbert102@gmail.com>
2024-05-01 09:42:20 -0400

    fix flake8

--- geostructures/utils/functions.py
+++ geostructures/utils/functions.py
@@ -2,6 +2,7 @@
 
 __all__ = ['round_half_up']
 
+
 def round_half_up(value: float, precision) -> float:
     """
     Rounds numbers to the nearest whole, where a value exactly between the two nearest
@@ -14,6 +15,6 @@ def round_half_up(value: float, precision) -> float:
             The precision to round the float value to
 
     """
-    mod= value + 10 ** -(precision +12)
-    
-    return round(mod,precision) 
+    mod = value + 10 ** -(precision + 12)
+
+    return round(mod, precision)
