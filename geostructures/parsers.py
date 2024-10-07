a41e29b02c139fae836eaf53dfd87ef983b55189
ccbest <carlcbest@gmail.com>
2024-10-07 11:33:07 -0400

    add docstrings

--- geostructures/parsers.py
+++ geostructures/parsers.py
@@ -1,18 +1,43 @@
 
+import json
 import re
-from typing import Any, Dict, Optional
+from typing import cast, Any, Dict, Optional, Union
 
 from geostructures.collections import FeatureCollection
 from geostructures.structures import GeoPolygon, GeoPoint, GeoLineString
 from geostructures.multistructures import MultiGeoPoint, MultiGeoPolygon, MultiGeoLineString
+from geostructures.typing import GeoShape
 
 
 def parse_geojson(
-    gjson: Dict[str, Any],
+    gjson: Union[str, Dict[str, Any]],
     time_start_property: str = 'datetime_start',
     time_end_property: str = 'datetime_end',
     time_format: Optional[str] = None,
 ):
+    """
+    Parses a GeoJSON structure into its corresponding geostructure(s).
+
+    Args:
+        gjson:
+            A GeoJSON structure (as a string or python dict)
+
+        time_start_property:
+            The geojson property containing the start time, if available
+
+        time_end_property:
+            The geojson property containing hte ned time, if available
+
+        time_format: (Optional)
+            The format of the timestamps in the above time fields.
+
+    Returns:
+        GeoShape, subtype determined by input
+    """
+    if isinstance(gjson, str):
+        gjson = json.loads(gjson)
+
+    gjson = cast(Dict[str, Any], gjson)
     parser_map = {
         'POINT': GeoPoint,
         'LINESTRING': GeoLineString,
@@ -40,7 +65,17 @@ def parse_geojson(
     )
 
 
-def parse_wkt(wkt: str):
+def parse_wkt(wkt: str) -> GeoShape:
+    """
+    Parses a WKT string into its corresponding geostructure.
+
+    Args:
+        wkt: (str)
+            A well known text string, representing a simplified geometry
+
+    Returns:
+        GeoShape, subtype determined by input
+    """
     parser_map = {
         'POINT': GeoPoint,
         'LINESTRING': GeoLineString,
